04e95dfe52295b0e3f9145bcfaa567d731a3f7c2
Hyejung Park <hyejj19@naver.com>
2023-01-12 18:14:39 +0900

    🔨 Modify:  로그 추가

--- src/api/searchAPI.ts
+++ src/api/searchAPI.ts
@@ -2,6 +2,7 @@ import { httpClient } from '../utils/httpClient';
 
 export const searchAPI = {
   getResult: (url: string) => {
+    console.info('calling api');
     return httpClient.get(url).then((res) => res.data);
   },
 };
