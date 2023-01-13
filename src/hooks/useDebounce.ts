caaa5e65a2091e6a98b88b0163accc729b1286e5
Hyejung Park <hyejj19@naver.com>
2023-01-13 11:55:17 +0900

    ♻️ Refactor :  타입 추가

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 
+type Callback = () => void;
+
 export const useDebounce = () => {
   const [timer, setTimer] = useState(0);
 
-  const debouncer = (refetch, delay) => {
+  const debouncer = (callback: Callback, delay: number) => {
     if (timer) {
       window.clearTimeout(timer);
     }
 
     const newTimer = window.setTimeout(() => {
       try {
-        refetch();
+        callback();
       } catch (e) {
         console.error('error', e);
       }
